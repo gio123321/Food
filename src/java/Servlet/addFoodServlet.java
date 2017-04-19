e13588a5634ac751baf28fb79720592f44453699
GabrielSanaia <gabriel.sanaia@gmail.com>
2017-04-19 23:38:23 +0400

    fixed empty request

--- src/java/Servlet/addFoodServlet.java
+++ src/java/Servlet/addFoodServlet.java
@@ -10,6 +10,8 @@ import java.io.File;
 import java.io.IOException;
 import java.io.PrintWriter;
 import java.util.ArrayList;
+import java.util.Collection;
+import java.util.Hashtable;
 import java.util.Iterator;
 import java.util.List;
 import java.util.logging.Level;
@@ -21,6 +23,7 @@ import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.Part;
 import org.apache.commons.fileupload.FileItem;
 import org.apache.commons.fileupload.FileUploadException;
 import org.apache.commons.fileupload.disk.DiskFileItemFactory;
@@ -36,6 +39,8 @@ public class addFoodServlet extends HttpServlet {
     private int maxMemSize = 5000 * 1024;
     private File file;
 
+//    private String name;
+//    private String cookingway;
     public void init() {
         // Get the file location where it would be stored.
         filePath
@@ -46,54 +51,21 @@ public class addFoodServlet extends HttpServlet {
     protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 
         try {
-            String fname = request.getParameter("name");
-            String ftype = request.getParameter("type");
-            FoodType type = FoodType.valueOf(ftype);
-            String cookingWay = request.getParameter("cookingway");
-            String quantity1 = request.getParameter("quantity");
-            int quantity = Integer.parseInt(quantity1);
-            ArrayList<Ingredient> ins = new ArrayList<>();
-            Ingredient in = null;
-            
-            for (int i = 1; i < quantity + 1; i++) {
-                
-                String iname = request.getParameter("name_" + i);
-                String iquantitystr = request.getParameter("quantity_" + i);
-                int iquantity = Integer.parseInt(iquantitystr);
-                String itype = request.getParameter("type_" + i);
-                String icomment = request.getParameter("comment_" + i);
-                
-                QuantityType intype = QuantityType.valueOf(itype);
-                
-                in = new Ingredient(iname, iquantity, intype, icomment);
-                ins.add(in);
-                
-                if (fname.isEmpty() || cookingWay.isEmpty()) {
-                    request.setAttribute("addFoodFailed", true);
-                    RequestDispatcher rd = request.getRequestDispatcher("addfood.jsp");
-                    rd.forward(request, response);
-                } else {
-                    Food d = new Food(ins, fname, type, cookingWay);
-                    FoodDAO dao = new FoodDAOImpl();
-                    dao.addFood(d);
-                }
-            }
-            
+
             isMultipart = ServletFileUpload.isMultipartContent(request);
             response.setContentType("text/html");
-            
+
             DiskFileItemFactory factory = new DiskFileItemFactory();
             // maximum size that will be stored in memory
             factory.setSizeThreshold(maxMemSize);
             // Location to save data that is larger than maxMemSize.
             factory.setRepository(new File("c:\\temp"));
-            
+
             // Create a new file upload handler
             ServletFileUpload upload = new ServletFileUpload(factory);
             // maximum file size to be uploaded.
             upload.setSizeMax(maxFileSize);
-            
-            
+
             // Parse the request to get file items.
             List fileItems = upload.parseRequest(request);
 
@@ -103,13 +75,13 @@ public class addFoodServlet extends HttpServlet {
             while (i.hasNext()) {
                 FileItem fi = (FileItem) i.next();
                 if (!fi.isFormField()) {
-                    // Get the uploaded file parameters
-                    String fieldName = fi.getFieldName();
+
+                    String fieldName = fi.getName() + " " + fi.getFieldName();
                     String fileName = fi.getName();
                     String contentType = fi.getContentType();
                     boolean isInMemory = fi.isInMemory();
                     long sizeInBytes = fi.getSize();
-                    // Write the file
+
                     if (fileName.lastIndexOf("\\") >= 0) {
                         file = new File(filePath
                                 + fileName.substring(fileName.lastIndexOf("\\")));
@@ -125,11 +97,61 @@ public class addFoodServlet extends HttpServlet {
 
                 }
             }
+
+            Hashtable table = new Hashtable();
+
+            Iterator<FileItem> iter = fileItems.iterator();
+            while (iter.hasNext()) {
+
+                FileItem item = iter.next();
+                if (!item.getFieldName().equals("file")) {
+                    table.put(item.getFieldName(), item.getString());
+
+                }
+            }
+
+            String name = (String) table.get("name");
+            String cookingway = (String) table.get("cookingway");
+            String typeStr = (String) table.get("type");
+            FoodType type = FoodType.valueOf(typeStr);
+            String inQuantity = (String) table.get("quantity");
+
+            int quan = Integer.parseInt(inQuantity);
+
+            ArrayList<Ingredient> ins = new ArrayList<>();
+
+            for (int n = 1; n < quan + 1; n++) {
+
+                String iname = (String) table.get("name_" + n);
+                String iquantitystr = (String) table.get("quantity_" + n);
+                int iquantity = Integer.parseInt(iquantitystr);
+                String itypeStr = (String) table.get("type_" + n);
+                String icomment = (String) table.get("comment_" + n);
+
+                QuantityType itype = QuantityType.valueOf(itypeStr);
+
+                Ingredient in = new Ingredient(iname, iquantity, itype, icomment);
+                ins.add(in);
+            }
+            if (name.isEmpty() || cookingway.isEmpty()) {
+                request.setAttribute("addFoodFailed", true);
+                RequestDispatcher rd = request.getRequestDispatcher("addfood.jsp");
+                rd.forward(request, response);
+
+            } else {
+
+                Food food = new Food(ins, name, type, cookingway);
+                FoodDAO dao = new FoodDAOImpl();
+                dao.addFood(food);
+            }
+
+        } catch (NumberFormatException ex) {
+            request.setAttribute("addFoodFailed", true);
+            RequestDispatcher rd = request.getRequestDispatcher("addfood.jsp");
+            rd.forward(request, response);
         } catch (FileUploadException ex) {
             Logger.getLogger(addFoodServlet.class.getName()).log(Level.SEVERE, null, ex);
         }
-
-       
 
     }
 
