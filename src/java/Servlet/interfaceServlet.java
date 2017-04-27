8212c91fa35b8424bfe7289de856939e9a794963
Giorgi <giorgigio740@gmail.com>
2017-04-27 19:45:16 +0400

    interface buttons fixed

--- src/java/Servlet/interfaceServlet.java
+++ src/java/Servlet/interfaceServlet.java
@@ -1,12 +1,26 @@
-
 package Servlet;
 
+import java.io.IOException;
+import java.io.PrintWriter;
+import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
-
-
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 
 public class interfaceServlet extends HttpServlet {
 
-   
+    @Override
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        String action = req.getParameter("1");
+        PrintWriter p = resp.getWriter();
+        if ("კერძის დამატება".equals(action)) {
+            p.append("1");
+        } else if ("მენიუს დამატება".equals(action)) {
+            p.append("2");
+        } else if ("მენიუს შერჩევა".equals(action)) {
+            p.append("3");
+        }
+    }
+
 }
