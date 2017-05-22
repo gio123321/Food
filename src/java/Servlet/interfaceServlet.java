b6a01cbecce4fb32aa307ae274c0536d68a29135
Ioseb Gobronidze <gobronidze.ioseb@gmail.com>
2017-05-22 23:19:54 +0400

    test commit...

--- src/java/Servlet/interfaceServlet.java
+++ src/java/Servlet/interfaceServlet.java
@@ -1,26 +1,14 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
 package Servlet;
 
-import java.io.IOException;
-import java.io.PrintWriter;
-import javax.servlet.RequestDispatcher;
-import javax.servlet.ServletException;
-import javax.servlet.http.HttpServlet;
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
-
-public class interfaceServlet extends HttpServlet {
-
-    @Override
-    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-        String id = request.getParameter("foodId");
-        response.setContentType("text/html");
-        response.setCharacterEncoding("UTF-8");
-        PrintWriter p = response.getWriter();
-        
-        
-        RequestDispatcher rd = request.getRequestDispatcher("food.jsp");
-        request.setAttribute("id",id);
-        
-        rd.forward(request, response);
-    }
+/**
+ *
+ * @author System Reserved
+ */
+public class InterfaceServlet {
+    
 }
