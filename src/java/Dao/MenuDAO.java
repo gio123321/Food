45fed3bd6c8d14374c269aee9cd6e5ea2f599c3c
GabrielSanaia <gabriel.sanaia@gmail.com>
2017-05-29 22:40:04 +0400

    added and implemented getMenu methods

--- src/java/Dao/MenuDAO.java
+++ src/java/Dao/MenuDAO.java
@@ -1,10 +1,17 @@
 package Dao;
 
+import Model.Food;
 import Model.Menu;
+import java.util.ArrayList;
 
 public interface MenuDAO {
 
     int addmenu(Menu menu);
     
     void fillFoods(Menu menu);
-}
+    
+    ArrayList<Menu> getMenus();
+    
+    ArrayList<Integer> getFoodIdsForMenu(Menu menu);
+   
+    }
